29449e00b1170b0b8fc9265261d70e74670dd93c
Oke Amuwha <amuwhao46@gmail.com>
2022-08-31 17:15:01 -0400

    Added api calls for Spotify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return null;
+
+  const AUTH_ENDPOINT = process.env.REACT_APP_AUTH_ENDPOINT;
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+  const RESPONSE_TYPE = process.env.REACT_APP_RESPONSE_TYPE;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Now Playing</h1>
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
+          Log in to Spotify
+        </a>
+      </header>
+    </div>
+    );
 }
 
 export default App;
+
